a5cf72a0992c0caae9b1152336f360f575942992
ossygarcia <oseghale@>
2022-02-17 19:30:09 +0100

    main

--- API/Server.js
+++ API/Server.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const corsOptions = {
+	// origin: 'http://localhost:3000',
+	credentials: true,
+	optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-	res.send("Welcome to Login System Server running");
-});
+const Port = process.env.PORT ||2000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+//Imports
+const Auth = require('./routes/auth');
+const UserRoute = require('./routes/user');
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+	.connect(process.env.MONGODB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		// useCreateIndex: false,
+		// useFindAndModify: false
+	})
+	.then(() => console.log('Database Connected Successfully !'))
+	.catch((err) => console.log(err));
+
+//Import initialization
+app.use('/api/Auth', Auth);
+app.use('/api/User', UserRoute);
+
+app.listen(Port, () => console.log(`Server Started on port ${Port}`));
